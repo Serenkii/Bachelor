8cb763567900e0d086d06b1157afe52b3bf260a0
Marian Gunsch <marian.gunsch@uni-konstanz.de>
2025-04-23 15:00:37 +0200

    Finished in office 21/04/25 - Plotted arrayjob stuff to compare

--- src/nernst_effect.py
+++ src/nernst_effect.py
@@ -5,53 +5,101 @@ import matplotlib.pyplot as plt
 
 import scipy as sp
 
+array_job_size = 10  # TODO: change to 10
+array_job_savefile = "data/nernst/arrj_"
 
-
-def average_arrayjob_data():
-    array_job_size = 10      # TODO: change to 10
+def read_arrayjob_data():
     base_path = "/data/scc/marian.gunsch/AM_tiltedX_ttmstep_7meV_2_id"  # index 1 to 10
     middle = "/spin-configs-99-999/mag-profile-99-999.altermagnet"
     suffix = ".dat"
-    skip = 1    # number of time steps to skip at the beginning
-    data_arrA = np.empty((array_job_size, 10000 - skip, 1537))     # 10 files, 10000 lines & 1537 columns per file
-    data_arrB = np.empty((array_job_size, 10000 - skip, 1537))
-    print("Loading data...")
+    data_arrA = np.empty((array_job_size, 10000, 1537))     # 10 files, 10000 lines & 1537 columns per file
+    data_arrB = np.empty((array_job_size, 10000, 1537))
+    print("Reading original data file...")
     for i in range(array_job_size):
         print(f"{i}", end="")
-        data_arrA[i] = np.loadtxt(f"{base_path}{i + 1}{middle}A{suffix}")[skip:, :]
+        data_arrA[i] = np.loadtxt(f"{base_path}{i + 1}{middle}A{suffix}")
         print("A", end="")
-        data_arrB[i] = np.loadtxt(f"{base_path}{i + 1}{middle}B{suffix}")[skip:, :]
+        data_arrB[i] = np.loadtxt(f"{base_path}{i + 1}{middle}B{suffix}")
         print("B")
 
+    print(f"Saving data to npy-files... ({array_job_savefile})")
+    np.save(f"{array_job_savefile}A.npy", data_arrA)
+    np.save(f"{array_job_savefile}B.npy", data_arrB)
 
-    sx_A = np.average(data_arrA[:, :, 1::3], axis=(0, 1))
-    sx_B = np.average(data_arrB[:, :, 1::3], axis=(0, 1))
-    sy_A = np.average(data_arrA[:, :, 2::3], axis=(0, 1))
-    sy_B = np.average(data_arrB[:, :, 2::3], axis=(0, 1))
-    sz_A = np.average(data_arrA[:, :, 3::3], axis=(0, 1))
-    sz_B = np.average(data_arrB[:, :, 3::3], axis=(0, 1))
 
-    return sx_A, sx_B, sy_A, sy_B, sz_A, sz_B
+def load_arrayjob_npy():
+    print("Loading data...")
+    A = np.load(f"{array_job_savefile}A.npy")
+    print("A", end="")
+    B = np.load(f"{array_job_savefile}B.npy")
+    print("B")
+    return A, B
+
+
+def array_job_neel_magn(data_arrA, data_arrB, plot=True):
+    skip_rows = 1
+
+    spin_z_A_tavg = np.average(data_arrA[:, skip_rows:, 3::3], axis=1)  # time average for each job
+    spin_z_B_tavg = np.average(data_arrB[:, skip_rows:, 3::3], axis=1)
+
+    neel_arr = 0.5 * (spin_z_A_tavg - spin_z_B_tavg)
+    magn_arr = 0.5 * (spin_z_A_tavg + spin_z_B_tavg)
+
+    neel = np.average(np.abs(neel_arr), axis=0)     # take average over the jobs, use absolute
+    magn = np.average(np.abs(magn_arr), axis=0)     # magnitude of vector for each
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.set_title("Neel and magnetization")
+        ax.set_xlabel("x as spin slot")
+        ax.set_ylabel("magnitude")
+        ax.plot(neel - 0.9525, label="|Neel| - 0.9525")
+        ax.plot(magn, label="|Magn|")
+        ax.legend()
+        plt.show()
+
+    return neel, magn
+
+
+def array_job_spin_current(data_arrA, data_arrB, plot=True):
+    skip_rows = 1
+
+    spin_x_A = data_arrA[:, skip_rows:, 1::3]
+    spin_x_B = data_arrB[:, skip_rows:, 1::3]
+    spin_y_A = data_arrA[:, skip_rows:, 2::3]
+    spin_y_B = data_arrB[:, skip_rows:, 2::3]
+
+    # intersublattice spin current
+    j_inter_1 = - (np.average(spin_x_A[:, :, :-1] * spin_y_A[:, :, 1:], axis=(0, 1))
+                   + np.average(spin_y_B[:, :, :-1] * spin_x_B[:, :, 1:], axis=(0, 1)))
+    j_inter_2 = - (np.average(spin_x_A[:, :, :-1] * spin_y_A[:, :, 1:], axis=(0, 1))
+                   - np.average(spin_y_B[:, :, :-1] * spin_x_B[:, :, 1:], axis=(0, 1)))
+
+    # inrtrasublattice spin current
+    j_intra_A = - (np.average(spin_x_A[:, :, :-1] * spin_y_A[:, :, 1:], axis=(0, 1))
+                   - np.average(spin_y_A[:, :, :-1] * spin_x_A[:, :, 1:], axis=(0, 1)))
+    j_intra_B = - (np.average(spin_x_B[:, :, :-1] * spin_y_B[:, :, 1:], axis=(0, 1))
+                   - np.average(spin_y_B[:, :, :-1] * spin_x_B[:, :, 1:], axis=(0, 1)))
+
+    if plot:
+        fig, ax = plt.subplots()
+        ax.set_title("Spin currents")
+        ax.set_xlabel("x as spin slot (or the place in beteen)")
+        ax.set_ylabel("magnitude")
+        ax.plot(j_inter_1, label="j_inter_1")
+        ax.plot(j_inter_2, label="j_inter_2")
+        ax.plot(j_intra_A, label="j_intra_A")
+        ax.plot(j_intra_B, label="j_intra_B")
+        ax.legend()
+        plt.show()
+
+    return j_inter_1, j_inter_2, j_intra_A, j_intra_B
+
 
 
 
 if __name__ == '__main__':
-    sx_A, sx_B, sy_A, sy_B, sz_A, sz_B = average_arrayjob_data()
-
-    neel = 0.5 * (sz_A - sz_B)
-    magn = 0.5 * (sz_A + sz_B)
-
-    fig, ax = plt.subplots()
-    ax.set_title("Neel")
-    ax.plot(neel)
-
-    plt.show()
-
-    fig, ax = plt.subplots()
-    ax.set_title("Magn")
-    ax.plot(magn)
-
-    plt.show()
-
-    # --> noise seems to be reduced by a about a factor 10
+    arr_datA, arr_datB = load_arrayjob_npy()
+    array_job_neel_magn(arr_datA, arr_datB, plot=True)
+    array_job_spin_current(arr_datA, arr_datB, plot=True)
 
